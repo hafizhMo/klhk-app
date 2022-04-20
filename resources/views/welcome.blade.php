5d30ca5128f1ea976006415e6a590a52931b2648
Krishna <krisnawirabuana@gmail.com>
2022-04-20 13:01:22 +0700

    Auto stash before merge of "master" and "origin/master"

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,9 +8,18 @@
         <title>Laravel</title>
 
     </head>
+<<<<<<< Updated upstream
     <body>
         <h1 class="text-3xl font-bold underline">Hello Yiren!</h1>
         <h1 class="text-3xl font-bold underline">Hello Yiren!</h1>
         <h1 class="text-3xl font-bold underline">Hello Yiren!</h1>
+=======
+    <body>  
+        <div class="h-48 columns-3">
+            <div class="h-24 w-full bg-gray-300">test 1</div>
+            <div class="h-24 w-full bg-red-300">test 2</div>
+            <div class="h-24 w-full bg-blue-500">test 3</div>
+        </div>
+>>>>>>> Stashed changes
     </body>
 </html>
